b4f8fe88ad4e355027faa7c79c74cd3a432ac4e7
Fremy Santana <frmysantana@yahoo.com>
2018-01-22 21:42:21 -0500

    Update scripts.js
    Improved the toggle feature. Now the other projects at least return when the selected project is clicked on again.

--- scripts.js
+++ scripts.js
@@ -1,20 +1,12 @@
 $(document).ready(function() {
-//   function hideProjectDetails(hoverEvent) {
-
-//   }
-
-  function showProjectDetails(hoverEvent) {
-    var projects = $("#Projects img");
-    for (i = 0; i < projects.length; i++) {
-        if (projects[i].id !== hoverEvent.target.id) {
-          $(projects[i].offsetParent).toggleClass("hide");
-        }
-    }
-    $(hoverEvent.currentTarget).removeClass().addClass("center");
-    var paragraph = $($(hoverEvent.currentTarget.offsetParent).children()[1]);
-    paragraph.removeClass("hide").attr("id", "project-description");
-  }
-
+  // Trying to store the original classes of the projects so that they
+  // may later be toggled
+  var originalClasses = $("#Projects img").classList;
+  $("#Projects img").map(function(i) {
+    console.log($(this)[i]);
+    // return el.classList;
+  });
+  
   // Toggle image and description when on mobile.  
   $("figure").click(function() {
       $("figure").addClass("vanish");
@@ -26,5 +18,26 @@ $(document).ready(function() {
       $("figure").removeClass("vanish");
   });
 
-  $("#Projects img").hover(showProjectDetails(e), hideProjectDetails(e));
+  // Toggle project description
+  $("#Projects img").click(function(e) {
+    var projects = $("#Projects img");
+    
+    // Toggle the other projects' visibilities
+    for (i = 0; i < projects.length; i++) {
+        if (projects[i].id !== e.target.id) {
+          $(projects[i]).parent().toggleClass("hide");
+        }
+    }
+  
+    // Toggle centering the clicked project and show description
+    if (!e.currentTarget.classList.contains("selected-project")) {
+      $(e.currentTarget).toggleClass();
+      $(e.currentTarget).addClass("selected-project");
+    } else {
+      $(e.currentTarget).toggleClass();
+    }
+
+    var paragraph = $($(e.currentTarget.offsetParent).children()[1]);
+    paragraph.toggleClass("hide");
+  });
 });
